3a93c5e1e2c4020755a0dc1fe4e8c10fe01a4d59
Chals <152137480+chals1029@users.noreply.github.com>
2025-11-18 01:07:44 +0800

    Delete GuillermosWebSystem directory

--- GuillermosWebSystem/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Redirect root to the landing page view in the MVC Views folder
-// Adjust this path if you later implement a front controller or routing system.
-header('Location: /GuillermosWebSystem/Views/landing/index.php');
-exit;
-?>
